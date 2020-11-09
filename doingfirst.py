9ed3d26d93da141c30b097f32bc2d5d1f29e3ecf
Vasanth <pvasanthk38@gmail.com>
2020-11-09 22:59:29 +0530

    Hey vasanth this is my new code

--- doingfirst.py
+++ doingfirst.py
@@ -6,3 +6,20 @@ print("This is vasanth new begging")
 print("we have done it cheers")
 
 print("two way comm")
+
+
+print("this is a new commit")
+
+
+print("trying something new")
+
+
+
+
+
+
+
+print("my life sucks")
+
+
+
